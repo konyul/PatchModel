d845f982bf6a99532e24364b478535665a79b468
dylee <dylee@spa.hanyang.ac.kr>
2024-07-08 17:59:49 +0900

    dy latest

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -6,8 +6,9 @@ from .encoder_decoder import EncoderDecoder
 from .multimodal_encoder_decoder import MultimodalEncoderDecoder
 from .seg_tta import SegTTAModel
 from .patch_encoder_decoder import Patch_EncoderDecoder
+from .patch_singlehead_encoder_decoder import Patch_singlehead_EncoderDecoder
 
 __all__ = [
     'BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'SegTTAModel',
-    'MultimodalEncoderDecoder', 'DepthEstimator', 'Patch_EncoderDecoder'
+    'MultimodalEncoderDecoder', 'DepthEstimator', 'Patch_EncoderDecoder', 'Patch_singlehead_EncoderDecoder'
 ]
