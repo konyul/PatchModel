02c3a169f36fa8804d1d228feee043219ae3bdfa
dylee <dylee@spa.hanyang.ac.kr>
2024-07-22 15:17:23 +0900

    dy latest

---
+++ configs/_base_/models/patchnet_freq_singlehead.py
@@ -0,0 +1,36 @@
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255)
+
+model = dict(
+    type='Patch_singlehead_EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    pretrained=None,
+    backbone=dict(
+        type='ResNet_freq',
+        num_res=3),
+
+    decode_head=dict(
+        type='PatchnetSingleHead',
+        in_channels=[64, 128, 256, 512],
+        in_index=[0, 1, 2, 3],
+        seg_head=True,
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=3,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        input_transform='multiple_select',
+        init_cfg=None,
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
-# CONFIG=patchnet_r10_1xb4_cityscapes-512x512
-CONFIG=patchnet_hyundae_512x512
+CONFIG=patchnet_hyundae_r10_512x512_wo_aug_lr5e-5_bs8_iter80k_singlehead #config name
 CONFIG_PATH=patchnet
 
-
-# CUDA_LAUNCH_BLOCKING=1 tools/dist_train.sh configs/$CONFIG_PATH/$CONFIG.py 1 --work-dir work_dir/$CONFIG_PATH/$CONFIG
-PORT=29501 tools/dist_test.sh configs/$CONFIG_PATH/$CONFIG.py ./work_dir/$CONFIG_PATH/$CONFIG/iter_16000.pth 1 
-
+PORT=29501 tools/dist_test.sh configs/$CONFIG_PATH/$CONFIG.py ./work_dir/$CONFIG_PATH/$CONFIG/iter_16000.pth 1 --out ./work_dir/$CONFIG_PATH/$CONFIG/pred_result.pkl
+python tools/analysis_tools/confusion_matrix.py configs/$CONFIG_PATH/$CONFIG.py ./work_dir/$CONFIG_PATH/$CONFIG/pred_result.pkl ./work_dir/$CONFIG_PATH/$CONFIG/confusion_matrix
