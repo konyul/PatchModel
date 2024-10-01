5b9e6799cdf86c3e296fd0592838cba43e2dc38a
SPA-junghokim <junghokim@spa.hanyang.ac.kr>
2024-10-02 04:24:13 +0900

    s5

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@
 #   PORT=29501 tools/dist_test.sh ./configs/patchnet/dylee/r34.py ./work_dir/$CONFIG_PATH/$CONFIG/iter_$ITER.pth 1 >> ./work_dir/$CONFIG_PATH/$CONFIG/iter_$ITER.txt
 # done
 
-PORT=29501 tools/dist_test.sh ./configs/patchnet/dylee/r34.py ./work_dirs/hmc_5000_2nd/dilation_1357_cls_w_30_2/iter_72000.pth 1 --out ./work_dirs/hmc_5000_2nd/dilation_1357_cls_w_30_2/pred_result.pkl
+PORT=2951 tools/dist_test.sh ./configs/patchnet_midterm/oldver_r34_pre_o_aug_o.py ./work_dir/patchnet_midterm/oldver_r34_pre_o_aug_o/iter_8000.pth 1 
 # python tools/analysis_tools/confusion_matrix.py configs/$CONFIG_PATH/$CONFIG.py ./work_dir/$CONFIG_PATH/$CONFIG/pred_result.pkl ./work_dir/$CONFIG_PATH/$CONFIG/confusion_matrix
